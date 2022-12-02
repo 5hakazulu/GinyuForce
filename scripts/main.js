3645824831c5e8f95006d74fdf2fa21bb08cf1ae
Joshua Zuluaga <zazuofcloud10@gmail.com>
2022-12-01 21:16:44 -0500

    fixed the image and search

--- scripts/main.js
+++ scripts/main.js
@@ -6,39 +6,40 @@ async function getData() {
     const parsedResponse = await result.json();
     console.log(parsedResponse)
 
-// async function getArtistData (){
-//     const result =  await fetch("https://api.genius.com/search?q=Kendrick%20Lamar&access_token=wG0T7APBwvyIwjbLlMF63S5NKLHV1o2UavANFobyb0xTf4ycHh_shy--Cf23G6Wj")
-//     const parsedResponse = await result.json();
-//     console.log(parsedResponse)
+    // async function getArtistData (){
+    //     const result =  await fetch("https://api.genius.com/search?q=Kendrick%20Lamar&access_token=wG0T7APBwvyIwjbLlMF63S5NKLHV1o2UavANFobyb0xTf4ycHh_shy--Cf23G6Wj")
+    //     const parsedResponse = await result.json();
+    //     console.log(parsedResponse)
 
 
-// }
+    // }
 
-// getArtistData();
+    // getArtistData();
 
-// async function getArtistEvent (){
-//     const 
-// }
+    // async function getArtistEvent (){
+    //     const 
+    // }
+}
 
- Promise.all([
+Promise.all([
     fetch("https://api.genius.com/search?q=Kendrick%20Lamar&access_token=wG0T7APBwvyIwjbLlMF63S5NKLHV1o2UavANFobyb0xTf4ycHh_shy--Cf23G6Wj")
         .then(value => value.json()),
     fetch("https://rest.bandsintown.com/artists/Kendrick%20Lamar/events?app_id=f902795fc209049a2c1bc9479048d797&date=upcoming")
         .then(value => value.json())
-    ])
+])
     .then((value) => {
-     console.log(value)
-     insert(value);
+        console.log(value)
+        insert(value);
     })
     .catch((err) => {
     });
 
 function insert(value) {
-  document.getElementById("cardContainer").innerHTML = renderArtistCard(value);
+    document.getElementById("cardContainer").innerHTML = renderArtistCard(value);
 }
 
 
-getData();
+
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -47,22 +48,19 @@ function getRandomInt(min, max) {
 }
 
 function setRandomBackgroundImage() {
-
-
     let randNum = getRandomInt(0, 4);
     console.log(randNum);
-
-
-
     document.getElementById("backgroundImage").src = `images/${bgImages[randNum]}`;
 }
+
+getData();
 
 setRandomBackgroundImage();
 
 function renderArtistCard(data) {
-  const artistArray = data.map(function (data) {
-    const index = artistData.map(object => object.englishName).indexOf(data.englishName)
-    return `<div class="col">
+    const artistArray = data.map(function (data) {
+        const index = artistData.map(object => object.englishName).indexOf(data.englishName)
+        return `<div class="col">
         <div class="card">
           <img src="${planetData[index].thumbImg}" class="card-img-top" alt="...">
           <div class="card-body">
@@ -71,27 +69,25 @@ function renderArtistCard(data) {
           </div>
         </div>
       </div>`;
-  });
-  return artistArray.join("");
+    });
+    return artistArray.join("");
 }
 
 
 document.addEventListener("DOMContentLoaded", function () {
     const myForm = document.getElementById("search-form");
     myForm.addEventListener("submit", async function (e) {
-      e.preventDefault();
-      const searchString = document.getElementById("search-bar").value;
-      const urlEncodedSearchString = encodeURIComponent(searchString);
-  
-      await fetch(`https://api.genius.com/search?q=${urlEncodedSearchString}s&access_token=wG0T7APBwvyIwjbLlMF63S5NKLHV1o2UavANFobyb0xTf4ycHh_shy--Cf23G6Wj`)
-      .then(async function(response){
-        return response.json();
-      })
-      .then(function(data){
-        document.getElementsByClassName("artist-container")[0].innerHTML = renderArtistCard(data.Search);
-        movieData = data.Search;
-      })
+        e.preventDefault();
+        const searchString = document.getElementById("search-bar").value;
+        const urlEncodedSearchString = encodeURIComponent(searchString);
+
+        await fetch(`https://api.genius.com/search?q=${urlEncodedSearchString}s&access_token=wG0T7APBwvyIwjbLlMF63S5NKLHV1o2UavANFobyb0xTf4ycHh_shy--Cf23G6Wj`)
+            .then(async function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                document.getElementsByClassName("artist-container")[0].innerHTML = renderArtistCard(data.Search);
+                movieData = data.Search;
+            })
     });
 })
-
-
